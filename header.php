f375afb048e42096d30ff73804f31f8a5c233599
katieshelly <katie.a.shelly@gmail.com>
2013-04-01 16:47:56 -0400

    small fix

--- header.php
+++ header.php
@@ -5,22 +5,5 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<link rel="stylesheet" type="text/css" media="all" href="../style.css" />
 </head>
-<body>
-
-
-<<<<<<< HEAD
-     {if 0}
-     <img src="{$cfg.abs_root_url}icon.png" height="55" width="55" />
-     {/if}
-
-     <h1>Object of the Day</h1>  
-     -->
-     
-<body>
-	<img src="http://media.cooperhewitt.org.s3.amazonaws.com/2013/03/01/Top-Banner.png"/>
-	<div class="TMSContent">Text pulled from TMS gets inserted here. The text would be the description field for the object.</div>
-	<div class="ObjShortURL">http://cprhwi.tt/o/2Ds7P</div>
-</body>
 </html>
-=======
->>>>>>> moved stuff around so it works in MAMP
+
